ee447272eb0af8c2903982308dec517420d648b8
Alexander Garin <garin1221w@gmail.com>
2016-06-11 23:20:09 +0500

    Fixes

--- public/js/share.js
+++ public/js/share.js
@@ -12,6 +12,15 @@ $(document).ready(function() {
     var vinNumber = getUrlParamValue('vinNumber');
     if (vinNumber) {
         if (vinNumber.length > 15 && vinNumber.length <= 21) {
+
+            var make = getUrlParamValue('make');
+            var model = getUrlParamValue('model');
+            var year = getUrlParamValue('carYear');
+            var price = getUrlParamValue('price');
+            var mileage = getUrlParamValue('mileage');
+            var fromCity = getUrlParamValue('fromCity');
+            var fromState = getUrlParamValue('fromState');
+
             // Open dialog share
             $('body').append("<!-- Share Modal -->" +
                 '<div class="modal fade" id="shareDialog" tabindex="-1" role="dialog" aria-labelledby="shareDialogTitle">' +
@@ -20,13 +29,13 @@ $(document).ready(function() {
                 '<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>' +
                 '<h4 class="modal-title" id="shareDialogTitle">Open in Application</h4></div>' +
                 '<div class="modal-body"><p>Detected vin number (' + vinNumber + ') that can be found in our mobile app</p>' +
-                '<p><b>Detail Information:</b></p>' +
-                '<p>Manufacturer: ' + getUrlParamValue('make') + '</p>' +
-                '<p>Model: ' + getUrlParamValue('model') + '</p>' +
-                '<p>Year: ' + getUrlParamValue('carYear') + '</p>' +
-                '<p><b>Price: ' + getUrlParamValue('price') + ' $</b></p>' +
-                '<p>Mileage: ' + getUrlParamValue('mileage') + ' miles</p>' +
-                '<p><b>Location: ' + getUrlParamValue('fromCity') + ', ' + getUrlParamValue('fromState') + '</b></p>' +
+                make && model && year && price && mileage && (fromCity || fromState) ? '<p><b>Detail Information:</b></p>' : '' +
+                make ? ('<p>Manufacturer: ' + make + '</p>') : '' +
+                model ? ('<p>Model: ' + model + '</p>') : '' +
+                year ? ('<p>Year: ' + year + '</p>') : '' +
+                price ? ('<p><b>Price: ' + price + ' $</b></p>') : '' +
+                mileage ? ('<p>Mileage: ' + mileage + ' miles</p>') : '' +
+                fromCity || fromState ? ('<p><b>Location: ' + (fromCity ? fromCity : '') + (fromState ? (fromCity ? fromCity : ', ')+fromState : '') + '</b></p>') : '' +
                 '<p><a class="btn btn-success" target="_blank" href="carswipe://' + vinNumber + '">Open in CarSwipe</a></p></div>' +
                 '<div class="modal-footer"><button type="button" class="btn btn-default" data-dismiss="modal">Close</button></div>' +
                 '</div></div></div></div>');
